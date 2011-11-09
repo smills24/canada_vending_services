34356b876215e5ab3308337498e99cc598ce456f
Steph <stephmills24@gmail.com>
2011-11-09 08:44:57 -0500

    css not precompiled

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,8 @@ module CanadaVendingMachines
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.assets.precompile += ['custom.css']
+
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
