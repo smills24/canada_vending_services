a6d2094f0a519320944ca1c3514cd0d8bb916a89
Steph <stephmills24@gmail.com>
2011-11-23 22:57:32 -0500

    mailer

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "gmail.com",
-  :user_name            => "stephmills24@gmail.com",
-  :password             => "kaninchen24",
+  :user_name            => "canadavending",
+  :password             => "canadavending1",
   :authentication       => :plain,
   :enable_starttls_auto => true
 }
