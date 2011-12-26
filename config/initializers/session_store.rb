bc23a9062a2ad445ec37a9fb8d1121a7a41fe0ed
Stephanie Mills <steph@The-TARDIS.local>
2011-12-26 11:59:42 -0500

    layout changes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-CanadaVendingMachines::Application.config.session_store :cookie_store, key: '_canada_vending_machines_session'
+CanadaVendingMachines::Application.config.session_store :cookie_store, :key => '_canada_vending_machines_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
